4729fb41b10c561438db5e31c44504eac8652d95
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-26 21:16:27 +0000

    Create the authentication handlers
    * Create the loginHandler and singupHandler to handle the authentication routes
    * Create a new multiplexer with the gorilla/mux package
    * Create and start a new sserver

--- cmd/processor-app/main.go
+++ cmd/processor-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+	"log"
 	"net/http"
 	"database/sql"
 	_"github.com/lib/pq"
@@ -11,14 +12,38 @@ import(
 var Db *sql.DB
 
 // Login Handler
-func login(w http.ResponseWriter, r *http.Request){}
+func loginHandler(w http.ResponseWriter, r *http.Request){}
+
+// Signup Handler
+func signupHandler(w http.ResponseWriter, r *http.Request){}
+
 
 func init(){
 	var err error
 
 	Db,err = sql.Open("postgres","user=growManage dbname=growManage password=fruitylupoooo1 sslmode=disable")
 	if err != nil{
-		panic(err)
+		log.Fatal("Error Creating Database Connection",err)
 	}
 
+}
+
+func main(){
+
+	router := mux.NewRouter()
+
+	// Handling routes with mux
+	router.HandleFunc("/login",loginHandler)
+	router.HandleFunc("/signup",signupHandler)
+
+	server:= http.Server{
+		Addr:"127.0.0.1:8080",
+		Handler:router,
+	}
+
+	err:=server.ListenAndServe()
+	
+	if err != nil {
+		log.Fatal("Error starting up server",err)
+	}
 }
